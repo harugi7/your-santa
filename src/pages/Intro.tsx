7e3e4249e7039838a2deda7db95fc4a61161f6f5
harugi7 <gimme2721@gmail.com>
2025-05-26 18:16:25 +0900

    feat: intro page

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,18 +1,33 @@
 import PageLayout from "../components/PageLayout.tsx";
 import styled from "styled-components";
+import Snowfall from "../components/Snowfall.tsx";
+import '../styles/fonts/fonts.css';
+import StartBtn from "../components/startBtn/StartBtn.tsx";
 
 function Intro() {
+
   return (
     <PageLayout>
-      <StyledTitle>너의 산타</StyledTitle>
+      <StyledTitleDiv>
+        <StyledMiniTitle>SANTA&apos;S</StyledMiniTitle>
+        <StyledMainTitle>MAGICAL</StyledMainTitle>
+        <StyledMainTitle>MAILROOM</StyledMainTitle>
+      </StyledTitleDiv>
+      <StartBtn/>
       <Dome />
+      <Snowfall/>
     </PageLayout>
   );
 }
 export default Intro;
 
-const StyledTitle = styled.div`
-  font-size: 100px;
+const StyledTitleDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const StyledMiniTitle = styled.div`
+  font-family: 'HARRYP', sans-serif;
+  font-size: 50px;
   font-weight: bold;
   color: white;
   position: relative;
@@ -20,7 +35,18 @@ const StyledTitle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+`;
+
+const StyledMainTitle = styled.div`
+  font-family: 'HARRYP', sans-serif;
+  font-size: 110px;
+  font-weight: bold;
+  color: white;
+  position: relative;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Dome = styled.div`
@@ -28,14 +54,14 @@ const Dome = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%) scale(1);
-    width: 300px;
-    height: 400px;
+    width: 400px;
+    height: 600px;
     background: linear-gradient(
             to bottom,
-            rgba(100, 140, 180, 0.85) 0%,
+            rgba(48, 111, 215, 0.85) 0%,
             rgba(27, 42, 65, 0) 100%
     );
-    border-radius: 150px 150px 0 0;
+    border-radius: 200px 200px 0 0;
     box-shadow: 0 0 20px 5px rgba(30, 50, 80, 0.7);
     z-index: 1;
     cursor: default;
