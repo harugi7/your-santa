273333bc784aef29265c37c576ac69c15689bbe3
harugi7 <gimme2721@gmail.com>
2025-05-26 18:16:11 +0900

    feat: add mouse diamond trail

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from "./routes";
+import MouseDiamondTrail from "./components/MouseDiamondTrail.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
+    <MouseDiamondTrail/>
   </StrictMode>,
 )
