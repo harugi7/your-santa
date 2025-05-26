1dd57fcdc7f97f37a3313201460fa0ed5a96852d
harugi7 <gimme2721@gmail.com>
2025-05-26 18:16:54 +0900

    feat: route guide page

---
+++ src/pages/Guide.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "../components/PageLayout.tsx";
+
+function Guide() {
+
+  return (
+    <PageLayout>
+      guide
+    </PageLayout>
+  );
+}
+export default Guide;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Intro from "../pages/Intro.tsx";
+import Guide from "../pages/Guide.tsx";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Intro />} />
+      <Route path="/guide" element={<Guide />} />
     </Routes>
   );
 }
