b804d614e80b05ff333ae128ad7bf753494bf96e
harugi7 <gimme2721@gmail.com>
2025-05-29 14:59:06 +0900

    feat: guide page

--- src/pages/Guide.tsx
+++ src/pages/Guide.tsx
@@ -1,11 +1,54 @@
 import PageLayout from "../components/PageLayout.tsx";
+import Background from "../components/Background.tsx";
+import styled from "styled-components";
+import StartBtn from "../components/button/StartBtn.tsx";
 
 function Guide() {
 
   return (
-    <PageLayout>
-      guide
+    <PageLayout backgroundOpacity={0.6}>
+      <Background/>
+      <CenterContainer>
+        <StyledMainText>
+          {`Send a REAL letter\n
+          from Santa, powered\n
+          by blabla magic`}
+        </StyledMainText>
+        <StyledSubText>
+          {`Answer three simple questions and\n
+          Santa will craft your letter to\n
+          download and print.`}
+        </StyledSubText>
+        <StartBtn/>
+      </CenterContainer>
     </PageLayout>
   );
 }
 export default Guide;
+
+const CenterContainer = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 40px;
+`;
+
+const StyledMainText = styled.div`
+    color: white;
+    font-size: 48px;
+    font-family: Georgia, serif;
+    white-space: pre-line;
+    line-height: 32px;
+`;
+
+const StyledSubText = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  font-size: 24px;
+  font-weight: 300;
+  font-family: sans-serif;
+  white-space: pre-line;
+  line-height: 16px;
+`;
